873fe55dbb4474f0a2b4c970098c835b43ba36c6
oleg <olegsaltuk@yandex.ru>
2019-07-09 13:54:23 +0300

    vue
    start project with Vue

---
+++ vue/components/logo.js
@@ -0,0 +1,6 @@
+Vue.component('logo', {
+    template: `<a href=".././index.html" class="logo">
+        <img src=".././img/logo.png" alt="logo__img">
+        bran<span class="logo__brand-color-d">d</span>
+    </a>`
+})
